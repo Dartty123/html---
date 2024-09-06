bd0f76f09eb46813235b6e66948932b13d07a79b
Dartty123 <dariyatamanuk@icloud.com>
2024-09-06 20:16:23 +0300

    М

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-    return render_template("index_pizza.html")
+    return render_template("index_pizza.html",title="ПІЦЕРІЯ")
 
 
 @app.get("/menu/")
